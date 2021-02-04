23b52ab13c76e88257cd51f4b01fb5f336d882d1
Martin Schwinzerl <martin.schwinzerl@cern.ch>
2021-02-04 17:28:37 +0100

    updates tracking implementation
    - adds maps for additional beam elements to the
      beam element dispatcher
    
    - fixes inconsistencies with sycl implementation

--- track.h
+++ track.h
@@ -15,7 +15,7 @@ namespace demotrack
     template< class LatticeArray >
     DEMOTRACK_EXTERN void Particle_track_until_turn(
         demotrack::Particle& p,
-        LatticeArray const& beam_elements,
+        LatticeArray const& lattice_buffer,
         demotrack::uint64_type const max_lattice_buffer_index,
         demotrack::int64_type const until_turn )
     {
@@ -29,15 +29,19 @@ namespace demotrack
             while( ( p.state == 1 ) && ( slot_idx < max_lattice_buffer_index ) )
             {
                 dt::beam_element_type const type_id = static_cast<
-                    dt::beam_element_type >( beam_elements[ slot_idx ] );
+                    dt::beam_element_type >( lattice_buffer[ slot_idx ] );
 
                 switch( type_id )
                 {
+                    /* all elements are stored with their type_id as the first
+                     * data member -> retrieve this number and dispatch
+                     * the track method accordingly */
+
                     case dt::BEAM_ELEMENT_DRIFT: // cf. beam_elements.h
                     {
                         const dt::Drift *const __restrict__ elem =
                             reinterpret_cast< dt::Drift const* >(
-                                &beam_elements[ slot_idx ] );
+                                &lattice_buffer[ slot_idx ] );
 
                         dt::uint64_type const next_slot_idx =
                             elem->track( p, slot_idx );
@@ -47,11 +51,26 @@ namespace demotrack
                         break;
                     }
 
+                    case dt::BEAM_ELEMENT_DRIFT_EXACT: // cf. beam_elements.h
+                    {
+                        const dt::DriftExact *const __restrict__ elem =
+                            reinterpret_cast< dt::DriftExact const* >(
+                                &lattice_buffer[ slot_idx ] );
+
+                        dt::uint64_type const next_slot_idx =
+                            elem->track( p, slot_idx );
+
+                        // Use GLOBAL_APERTURE_CHECK from Drift -> it's the same
+                        dt::Drift::GLOBAL_APERTURE_CHECK( p );
+                        slot_idx = next_slot_idx;
+                        break;
+                    }
+
                     case dt::BEAM_ELEMENT_MULTIPOLE: // cf. beam_elements.h
                     {
                         const dt::Multipole *const __restrict__ elem =
                             reinterpret_cast< dt::Multipole const* >(
-                                &beam_elements[ slot_idx ] );
+                                &lattice_buffer[ slot_idx ] );
 
                         dt::uint64_type const next_slot_idx =
                             elem->track( p, slot_idx );
@@ -61,17 +80,89 @@ namespace demotrack
                         break;
                     }
 
+                    case dt::BEAM_ELEMENT_XY_SHIFT: // cf. beam_elements.h
+                    {
+                        const dt::XYShift *const __restrict__ elem =
+                            ( dt::XYShift const* )&lattice_buffer[ slot_idx ];
+
+                        dt::uint64_type const next_slot_idx =
+                            elem->track( p, slot_idx );
+
+                        slot_idx = next_slot_idx;
+                        break;
+                    }
+
+                    case dt::BEAM_ELEMENT_SROTATION: // cf. beam_elements.h
+                    {
+                        const dt::SRotation *const __restrict__ elem =
+                            ( dt::SRotation const* )&lattice_buffer[ slot_idx ];
+
+                        dt::uint64_type const next_slot_idx =
+                            elem->track( p, slot_idx );
+
+                        slot_idx = next_slot_idx;
+                        break;
+                    }
+
                     case dt::BEAM_ELEMENT_CAVITY: // cf. beam_elements.h
                     {
                         const dt::Cavity *const __restrict__ elem =
                             reinterpret_cast< dt::Cavity const* >(
-                                &beam_elements[ slot_idx ] );
+                                &lattice_buffer[ slot_idx ] );
 
                         dt::uint64_type const next_slot_idx =
                             elem->track( p, slot_idx );
 
                         slot_idx = next_slot_idx;
                         ++p.at_element;
+                        break;
+                    }
+
+                    case dt::BEAM_ELEMENT_LIMIT_RECT: // cf. beam_elements.h
+                    {
+                        const dt::LimitRect *const __restrict__ elem =
+                            ( dt::LimitRect const* )&lattice_buffer[ slot_idx ];
+
+                        dt::uint64_type const next_slot_idx =
+                            elem->track( p, slot_idx );
+
+                        slot_idx = next_slot_idx;
+                        break;
+                    }
+
+                    case dt::BEAM_ELEMENT_LIMIT_ELLIPSE: // cf. beam_elements.h
+                    {
+                        const dt::LimitEllipse *const __restrict__ elem =
+                            ( dt::LimitEllipse const* )&lattice_buffer[ slot_idx ];
+
+                        dt::uint64_type const next_slot_idx =
+                            elem->track( p, slot_idx );
+
+                        slot_idx = next_slot_idx;
+                        break;
+                    }
+
+                    case dt::BEAM_ELEMENT_LIMIT_RECT_ELLIPSE: // cf. beam_elements.h
+                    {
+                        const dt::LimitRectEllipse *const __restrict__ elem =
+                            ( dt::LimitRectEllipse const* )&lattice_buffer[ slot_idx ];
+
+                        dt::uint64_type const next_slot_idx =
+                            elem->track( p, slot_idx );
+
+                        slot_idx = next_slot_idx;
+                        break;
+                    }
+
+                    case dt::BEAM_ELEMENT_DIPEDGE: // cf. beam_elements.h
+                    {
+                        const dt::DipoleEdge *const __restrict__ elem =
+                            ( dt::DipoleEdge const* )&lattice_buffer[ slot_idx ];
+
+                        dt::uint64_type const next_slot_idx =
+                            elem->track( p, slot_idx );
+
+                        slot_idx = next_slot_idx;
                         break;
                     }
 
@@ -82,7 +173,7 @@ namespace demotrack
                     {
                         const dt::SpaceChargeCoasting *const __restrict__ elem =
                             reinterpret_cast< dt::SpaceChargeCoasting const* >(
-                                &beam_elements[ slot_idx ] );
+                                &lattice_buffer[ slot_idx ] );
 
                         dt::uint64_type const next_slot_idx =
                             elem->track( p, slot_idx );
